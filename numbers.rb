a00e5a4affe96a2beffe4793ceaaf1158b8225ee
zacck <coderv63@gmail.com>
2016-02-18 09:36:03 +0000

    comparison operators and basic looping

--- numbers.rb
+++ numbers.rb
@@ -1,27 +1,44 @@
+#adds nummbers
 puts 1+3
 
+#mmultiplies 2 numbers
 puts 3 *5
 
+#integer divider
 puts 15/5
 
+#divider to get decimals
 puts 20/3.to_f
 
+#greater than comparison 
 puts 10 > 10
 
+#compares 2 values to equality
 puts  10 == 12
 
-#generate random numbers
+#generate random numbers between 0 and n -1
 puts rand(10)
 
+#checks if there is a remainder
+puts 20 % 3 
+
+#check for odd
+puts 22.odd?
+ 
+#check if number is even 
+puts 22.even?
+
+#convert float to integer 
+puts 12.0.to_i
+
+#convert string to integer 
+puts "5".to_i
+
+#convert integer to string 
+puts 5.to_i
 
 
-puts "enter first number"
-first_number = gets.chomp
-puts "please enter second number"
-second_number = gets.chomp
+#looping 
+20.times{puts "Hello World"}
 
-first = first_number.to_i
-second = second_number.to_i
-
-puts first * second
-
+20.times{puts 10}
