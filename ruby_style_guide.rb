176a063609f432f11ab40a5d74f129f3fa6cc247
zacck <coderv63@gmail.com>
2016-03-09 11:49:09 +0000

    went through the ruby style guide

---
+++ ruby_style_guide.rb
@@ -0,0 +1,12 @@
+#from https://github.com/bbastov/ruby-style-guide
+
+def hello_people(param_date, param_where)
+  me = {'name'=>'Zacck',
+         'statement'=>'Hello I am here to improve my webdevelopment',
+          'from'=>'South Africa'}
+  puts me["name"]
+end
+
+hello_people("today", "here")
+
+
