a6911bc578c97b1f78260e523408702d87128b19
zacck <coderv63@gmail.com>
2016-03-12 22:53:52 +0000

    Added a route , a template, a controller and an action

--- rails_samples/hello_world/config/routes.rb
+++ rails_samples/hello_world/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  get'welcome/home', to: 'welcome#home'
+  get'welcome/about', to: 'welcome#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
